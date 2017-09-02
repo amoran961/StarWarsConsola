960456fbe859c33effc3da8f25f8dab92a1a688d
amoran961 <anthony_mm_96@hotmail.com>
2017-09-02 13:14:24 -0500

    Settings fix 1

--- StarWars/settings.py
+++ StarWars/settings.py
@@ -14,9 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-PROJECT_ROOT  = os.path.normpath(os.path.join(BASE_DIR,__file__))
+PROJECT_ROOT  = os.path.normpath(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,7 +125,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, ''),
+    os.path.join(PROJECT_ROOT, 'static'),
 )
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
