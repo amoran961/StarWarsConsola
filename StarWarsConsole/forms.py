52eca76d04e53174cbc58ef285bfb0f9d6a9caf9
20113032 <flavio_g_r_p.93@hotmail.com>
2017-09-23 15:28:01 -0500

    corregido importacion en forms

--- StarWarsConsole/forms.py
+++ StarWarsConsole/forms.py
@@ -2,7 +2,7 @@ import re
 from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
-from posts.models import Usuario
+from StarWarsConsole.models import Usuario
 
 class RegistrationForm(forms.Form):
     usuario = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=True, max_length=30)), label=_("Usuario"), error_messages={ 'invalid': _("Usuario sólo puede tener letras, números y _.") })
