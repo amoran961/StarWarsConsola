7a86ae871ec32658fede95ca5691004aea5c9556
20113032 <flavio_g_r_p.93@hotmail.com>
2017-09-23 15:54:38 -0500

    actualizar urls

--- StarWarsConsole/urls.py
+++ StarWarsConsole/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.views import login
 from StarWarsConsole.views import register, register_success, logout_page, home
 
 urlpatterns = [
-    url(r'^/$', logout_page, name='logout_page'),
-    url(r'^/$', login, name='login'),
+    url(r'^$', logout_page, name='logout_page'),
+    url(r'^$', login, name='login'),
     url(r'^register/$', register, name='register'),
     url(r'^register/success/$', register_success, name='register_success'),
     url(r'^home/$', home, name='home')
