83fd83d6a6708c8f7c1403b43bcda9b756b4df0c
20113032 <flavio_g_r_p.93@hotmail.com>
2017-09-23 19:33:29 -0500

    3

--- StarWarsConsole/views.py
+++ StarWarsConsole/views.py
@@ -42,8 +42,8 @@ def login(request):
     if request.method == 'POST':
         form=LoginForm(request.POST)
         if form.is_valid():
-            username = request.POST['username']
-            password = request.POST['password']
+            username = request.POST.get['username']
+            password = request.POST.get['password']
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 login(request, user)
