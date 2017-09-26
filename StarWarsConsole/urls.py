c7021b240beb7508016499bb1e76ed869d0998da
20113032 <flavio_g_r_p.93@hotmail.com>
2017-09-25 21:58:26 -0500

    se agregaron urls

--- StarWarsConsole/urls.py
+++ StarWarsConsole/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
-from StarWarsConsole.views import register, register_success, logout_page, home, authlogin
+from StarWarsConsole.views import register, register_success, logout_page, home, authlogin, login_juego, register_juego
 
 urlpatterns = [
     url(r'^logout/$', logout_page, name='logout_page'),
@@ -8,5 +8,7 @@ urlpatterns = [
     url(r'^accounts/login/$', authlogin, name='login'),
     url(r'^register/$', register, name='register'),
     url(r'^register/success/$', register_success, name='register_success'),
-    url(r'^home/$', home, name='home')
+    url(r'^home/$', home, name='home'),
+    url(r'^login_juego/$', login_juego),
+    url(r'^register_juego/$', register_juego),
 ]
