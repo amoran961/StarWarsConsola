0a1dd1f25ccf63d29e85c267c004d4cb759452a5
amoran961 <20132982@aloe.ulima.edu.pe>
2017-11-29 01:08:09 -0500

    import login

--- StarWars/urls.py
+++ StarWars/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from StarWarsConsole.views import authlogin
 
 urlpatterns = [ url(r'^accounts/login/$', authlogin, name='login'),
     url(r'^StarWarsConsole/', include('StarWarsConsole.urls')),
